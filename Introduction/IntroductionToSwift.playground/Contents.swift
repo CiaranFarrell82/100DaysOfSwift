9c283542b12ed03caee4a83ea22483c0cdcf6b2d
CiaranFarrell82 <ciaran.farrell82@mail.dcu.ie>
2021-02-13 13:27:18 +0000

    adding 100 days of swift

---
+++ Introduction/IntroductionToSwift.playground/Contents.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+/*
+ * 100 Days of Swift - Day 1 🎉 ------------------------------------------------------------------
+ * Topic: variables, simple data types, and string interpolation
+ * 13th Feb 2021 📅
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+* 100 Days of Swift - Day 2 🎉 ------------------------------------------------------------------
+* Topic: 
+* 13th Feb 2021 📅
+*/
---
+++ README.md
@@ -0,0 +1,8 @@
+![100 Days of Swift](https://github.com/ciaranfarrell82/100DaysOfSwift/blob/master/image.jpg)
+
+## [100 Days of Swift](https://www.hackingwithswift.com/100) by Paul Hudson (hackingwithswift.com)
+
+##  Days 1-12: Introduction to Swift
+
+-  Day 1:  variables, simple data types, and string interpolation 🎉
+
