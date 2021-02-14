6fe11c6a305630ea144a9436434709a2c6b02012
CiaranFarrell82 <ciaran.farrell82@mail.dcu.ie>
2021-02-14 12:30:57 +0000

    completed day 1

--- Introduction/IntroductionToSwift.playground/Contents.swift
+++ Introduction/IntroductionToSwift.playground/Contents.swift
@@ -6,20 +6,46 @@ import UIKit
  * 13th Feb 2021 📅
  */
 
-let name = "Ciaran"
-print("my name is \(name)")
 
+// variables
+var str = "Hello, playground"
+str = "Goodbye"
 
+// strings and integers
+var age = 38
+var population = 8_000_000
 
+// multi-line strings
+var str1 = """
+This goes
+over multiple
+lines
+"""
 
+var str2 = """
+This goes \
+over multiple \
+lines
+"""
 
+// doubles and booleans
+var pi = 3.141
+var awesome = true
 
+// string interpolation
+var score = 85
+var str3 = "Your score was \(score)"
+var results = "The test results are here: \(str3)"
 
+// constants
+let taylor = "swift"
 
-
-
-
-
+// type annotations
+let str4 = "Hello, playground"
+let album: String = "Reputation"
+let year: Int = 1989
+let height: Double = 1.78
+let taylorRocks: Bool = true
 
 /*
 * 100 Days of Swift - Day 2 🎉 ------------------------------------------------------------------
