fb579ddb88da8646b71d43160d0ac378b444d3c2
CiaranFarrell82 <ciaran.farrell82@mail.dcu.ie>
2021-02-15 00:32:12 +0000

    completed day 2

--- Introduction/IntroductionToSwift.playground/Contents.swift
+++ Introduction/IntroductionToSwift.playground/Contents.swift
@@ -50,5 +50,120 @@ let taylorRocks: Bool = true
 /*
 * 100 Days of Swift - Day 2 🎉 ------------------------------------------------------------------
 * Topic: arrays, dictionaries, sets, and enums
-* 13th Feb 2021 📅
+* 14th Feb 2021 📅
 */
+
+// arrays
+let john = "John Lennon"
+let paul = "Paul McCartney"
+let george = "George Harrison"
+let ringo = "Ringo Starr"
+
+let beatles = [john, paul, george, ringo]
+beatles[1]
+
+// sets
+let colors = Set(["red", "green", "blue"])
+
+// tuples
+var name = (first: "Taylor", last: "Swift")
+name.0
+name.first
+
+// arrays vs sets vs tuples
+
+// tuple
+let address = (house: 555, street: "Taylor Swift Avenue", city: "Nashville")
+
+// set
+let set = Set(["aardvark", "astronaut", "azalea"])
+
+// array
+let pythons = ["Eric", "Graham", "John", "Michael", "Terry", "Terry"]
+
+// dictonaries
+let heights = [
+    "Taylor Swift": 1.78,
+    "Ed Sheeran": 1.73
+]
+
+heights["Taylor Swift"]
+
+// dictionary default values
+let favoriteIceCream = [
+    "Paul": "Chocolate",
+    "Sophie": "Vanilla"
+]
+
+favoriteIceCream["Paul"]
+favoriteIceCream["Charlotte"]
+
+favoriteIceCream["Charlotte", default: "Unknown"]
+
+// creating empty collections
+var teams = [String: String]()
+
+teams["Paul"] = "Red"
+
+var results2 = [Int]()
+
+var words = Set<String>()
+var numbers = Set<Int>()
+
+var scores = Dictionary<String, Int>()
+var results3 = Array<Int>()
+
+// enumerations (enums)
+let result = "failure"
+let result2 = "failed"
+let result3 = "fail"
+
+enum Result {
+    case success
+    case failure
+}
+
+let result4 = Result.failure
+
+// enum asscociated values
+enum Activity {
+    case bored
+    case running
+    case talking
+    case singing
+}
+
+enum Activity2 {
+    case bored
+    case running(destination: String)
+    case talking(topic: String)
+    case singing(volume: Int)
+}
+
+let talking = Activity2.talking(topic: "football")
+
+// enum raw values
+enum Planet: Int {
+    case mercury
+    case venus
+    case earth
+    case mars
+}
+
+let earth = Planet2(rawValue: 2)
+
+enum Planet2: Int {
+    case mercury = 1
+    case venus
+    case earth
+    case mars
+}
+
+/*
+* 100 Days of Swift - Day 3 🎉 ------------------------------------------------------------------
+* Topic: operators and conditions
+* 14th Feb 2021 📅
+*/
+
+
+
