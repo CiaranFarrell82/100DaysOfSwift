f3cde318d298c6d026ae10c8c999e724c8ba8953
CiaranFarrell82 <ciaran.farrell82@mail.dcu.ie>
2021-02-15 13:16:54 +0000

    50% completed day 3

--- Introduction/IntroductionToSwift.playground/Contents.swift
+++ Introduction/IntroductionToSwift.playground/Contents.swift
@@ -165,5 +165,71 @@ enum Planet2: Int {
 * 14th Feb 2021 📅
 */
 
+// arithmetic operators
+let firstScore = 12
+let secondScore = 4
+
+// add (+) & subtract (-)
+let total = firstScore + secondScore
+let difference = firstScore - secondScore
+
+// multiply (*) & divide (/)
+let product = firstScore * secondScore
+let divided = firstScore / secondScore
+
+let remainder = 13 % secondScore
+
+// operator overloading
+let meaningOfLife = 42
+let doubleMeaning = 42 + 42
+
+let fakers = "Fakers gonna "
+let action = fakers + "fake"
+
+let firstHalf = ["John", "Paul"]
+let secondHalf = ["George", "Ringo"]
+let beatles2 = firstHalf + secondHalf
+
+// compound assignment operators
+var score2 = 95
+score2 -= 5
+
+var quote = "The rain in Spain falls mainly on the "
+quote += "Spaniards"
+
+// comparison operators
+let firstScore2 = 6
+let secondScore2 = 4
+
+firstScore2 == secondScore2
+firstScore2 != secondScore2
+
+firstScore2 < secondScore2
+firstScore2 >= secondScore2
+
+"Taylor" <= "Swift"
+
+// conditions
+let firstCard = 11
+let secondCard = 10
+
+if firstCard + secondCard == 21 {
+    print("Blackjack!")
+}
+
+if firstCard + secondCard == 21 {
+    print("Blackjack!")
+} else {
+    print("Regular cards")
+}
+
+if firstCard + secondCard == 2 {
+    print("Aces - lucky!")
+} else if firstCard + secondCard == 21 {
+    print("Blackjack!")
+} else {
+    print("Regular cards")
+}
+
 
 
